50a264c50569ea27c7544cb17787309df6feb569
matjemeisje <matje@taalmonsters.nl>
2016-02-28 11:58:45 +0100

    Add sentiment columns to facebook status index

--- config/initializers/update_statuses.rb
+++ config/initializers/update_statuses.rb
@@ -1,25 +1,38 @@
-batch_size = 1000
-0.step(TwitterStream.count, batch_size).each do |offset|
-  TwitterStream.order(:id)
-               .offset(offset)
-               .limit(batch_size)
-               .update_all(:status => 2)
-end
-0.step(TwitterSearch.count, batch_size).each do |offset|
-  TwitterSearch.order(:id)
-               .offset(offset)
-               .limit(batch_size)
-               .update_all(:status => 2)
-end
-0.step(TwitterIdSearch.count, batch_size).each do |offset|
-  TwitterIdSearch.order(:id)
-               .offset(offset)
-               .limit(batch_size)
-               .update_all(:status => 2)
-end
-0.step(FacebookSearch.count, batch_size).each do |offset|
-  FacebookSearch.order(:id)
-               .offset(offset)
-               .limit(batch_size)
-               .update_all(:status => 2)
+begin
+  ActiveRecord::Base.connection
+  batch_size = 1000
+  if ActiveRecord::Base.connection.table_exists? 'twitter_streams'
+    0.step(TwitterStream.count, batch_size).each do |offset|
+      TwitterStream.order(:id)
+                   .offset(offset)
+                   .limit(batch_size)
+                   .update_all(:status => 2)
+    end
+  end
+  if ActiveRecord::Base.connection.table_exists? 'twitter_searches'
+    0.step(TwitterSearch.count, batch_size).each do |offset|
+      TwitterSearch.order(:id)
+                   .offset(offset)
+                   .limit(batch_size)
+                   .update_all(:status => 2)
+    end
+  end
+  if ActiveRecord::Base.connection.table_exists? 'twitter_id_searches'
+    0.step(TwitterIdSearch.count, batch_size).each do |offset|
+      TwitterIdSearch.order(:id)
+                   .offset(offset)
+                   .limit(batch_size)
+                   .update_all(:status => 2)
+    end
+  end
+  if ActiveRecord::Base.connection.table_exists? 'facebook_searches'
+    0.step(FacebookSearch.count, batch_size).each do |offset|
+      FacebookSearch.order(:id)
+                   .offset(offset)
+                   .limit(batch_size)
+                   .update_all(:status => 2)
+    end
+  end
+rescue ActiveRecord::NoDatabaseError
+  
 end
