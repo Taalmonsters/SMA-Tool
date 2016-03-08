c0f190c8e08ad248221be3e209c747aaa3a7a324
matjemeisje <matje@taalmonsters.nl>
2016-03-08 23:14:41 +0100

    Update

--- app/models/twitter_search.rb
+++ app/models/twitter_search.rb
@@ -64,12 +64,15 @@ class TwitterSearch < ActiveRecord::Base
             end
             sleep(5 * User.find(self.user_id).active_twitter_threads)
           end
-        # rescue => e
-          # p e.message
-          # p e.backtrace.join("\n")
+          if i == 0
+            self.update_attribute(:status, :finished)
+          end
+        rescue => e
+          p e.message
+          p e.backtrace.join("\n")
         end while url != nil && !terminated
       end
-      self.update_attribute(:status, :finished)
+      # self.update_attribute(:status, :finished)
     end
   end
   
